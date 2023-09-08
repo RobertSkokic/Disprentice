bc3617780d00e01028015f6aade4740404c38b89
robi <robi.skokic@hotmail.com>
2023-09-08 14:38:58 +0200

    last push

--- scripts.js
+++ scripts.js
@@ -13,3 +13,19 @@ function closeModal(modalId) {
         modal.style.display = "none";
     }, 300);
 }
+
+document.getElementById('search-button').addEventListener('click', function() {
+    // Holen Sie sich den Suchtext
+    const searchText = document.querySelector('.form-control').value.toLowerCase();
+
+    // Durchlaufen Sie alle Kacheln
+    const tiles = document.querySelectorAll('.category-card');
+    tiles.forEach(tile => {
+        const title = tile.querySelector('h2').textContent.toLowerCase();
+        if (title.includes(searchText)) {
+            tile.style.display = 'block'; // Zeigen Sie die Kachel an, wenn sie dem Suchtext entspricht
+        } else {
+            tile.style.display = 'none'; // Verbergen Sie die Kachel, wenn sie nicht dem Suchtext entspricht
+        }
+    });
+});
