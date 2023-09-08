8920958cf0bb18551def62b6d00804f35c54efd4
robi <robi.skokic@hotmail.com>
2023-09-08 13:59:36 +0200

    test

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 function openModal(modalId) {
-    document.getElementById(modalId).style.display = "block";
+    const modal = document.getElementById(modalId);
+    modal.style.display = "flex";
+    setTimeout(() => {
+        modal.classList.add('show');
+    }, 50);
 }
 
 function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
+    const modal = document.getElementById(modalId);
+    modal.classList.remove('show');
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 300);
 }
